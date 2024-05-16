abbea13bb89e6dedffd77ad3be1ef30969672bc5
Brian Stern <sterno2510@gmail.com>
2024-05-16 12:49:19 -0400

    added ideas

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ function App() {
   return (
     <div className="App">
      <h1>AI Companion</h1>
+     <div>Update Resume</div>
+     <div>Create Cover Letter</div>
+     <div>Extract Audio from Video</div>
     </div>
   );
 }
