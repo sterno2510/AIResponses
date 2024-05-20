8c338dd31f9a8dd9d836346e9b431e537269fc37
Brian Stern <sterno2510@gmail.com>
2024-05-20 19:48:08 -0400

    set up react router for transcribe path

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ErrorPage from './error-page';
+import VideoUpload from './VideoUpload';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: 'transcribe/',
+    element: <VideoUpload />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
 
