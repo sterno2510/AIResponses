72f1ae6411aabe9861e0f7774515beb608daa839
Brian Stern <sterno2510@gmail.com>
2024-05-25 15:45:37 -0400

    removed google speech api and only using openai for transcribing and formatting the text

--- src/TranscriptionText.jsx
+++ src/TranscriptionText.jsx
@@ -1,26 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import axios from 'axios';
 
 function TranscriptionText({ text }) {
-  const [processedText, setProcessedText] = useState('');
-
-  useEffect(() => {
-    const prompt = {
-      prompt: `Analyze the following text and add appropriate paragraph breaks with <br/> and punctuation: ${text}.`,
-    };
-    axios.post('/api/openai', prompt)
-      .then((success) => {
-        console.log('success', success.data);
-        setProcessedText(success.data.message.content);
-      })
-      .catch((error) => {
-        console.log(`Error making network call to openAI ${error}`);
-      });
-  }, [text]);
-
   return (
-    <div dangerouslySetInnerHTML={{ __html: processedText }} />
+    <div dangerouslySetInnerHTML={{ __html: text }} />
   );
 }
 
