92e2163fa5d8561203192635c9122d825bec06e6
Brian Stern <sterno2510@gmail.com>
2024-05-30 15:52:15 -0400

    connected resume component to server route

--- src/Resume.jsx
+++ src/Resume.jsx
@@ -4,12 +4,18 @@ import axios from 'axios';
 
 // create function to send text data to backend
 const hello = { body: 'hello' };
-const uploadResume = (text) => {
+
+const uploadResume = () => {
+  console.log('in upload');
   axios.post('/api/openai/resume', hello);
 };
+
 function Resume() {
   return (
-    <div>this is where you update your resume</div>
+    <>
+      <div>this is where you update your resume</div>
+      <button onClick={uploadResume} type="submit">Test</button>
+    </>
   );
 }
 
