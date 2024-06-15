4f4eda8ccc0f4f5a4c917227dc6e1ca61f8b84fd
Brian Stern <sterno2510@gmail.com>
2024-06-14 20:00:52 -0400

    pulled styled components from other components

---
+++ src/FormGroupStyledComponents.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+export const LabelStyled = styled.label`
+  font-weight: bold;
+  margin-bottom: 5px;
+  display: block;
+`;
+
+export const FormGroupStyled = styled.div`
+  margin-bottom: 20px;
+`;
+
+export const InputStyled = styled.input`
+  width: 100%;
+  padding: 10px;
+  margin-top: 5px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+`;
+
+export const TextAreaStyled = styled.textarea`
+  width: 100%;
+  padding: 10px;
+  margin-top: 5px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  min-height: 100px; /* Adjust the height as needed */
+`;
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,28 +1,12 @@
 /* eslint-disable react/function-component-definition */
 import React from 'react';
-import styled from 'styled-components';
 import logo from './assets/ailogo.png';
 import slogan from './assets/slogan.png';
-
-const HeaderContainerStyled = styled.header`
-  background-color: #404040;
-  padding: 10px 20px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border-bottom: 5px solid #0068d1;
-`;
-
-const LogoStyled = styled.img`
-  max-height: 80px;
-  width: auto;
-  margin-right: 10px; /* Adjust margin between logo and slogan */
-`;
-
-const SloganStyled = styled.img`
-  max-height: 75px;
-  width: auto;
-`;
+import {
+  HeaderContainerStyled,
+  LogoStyled,
+  SloganStyled,
+} from './HeaderStyledComponents';
 
 const Header = () => (
   <HeaderContainerStyled>
