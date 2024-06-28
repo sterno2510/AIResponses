1dfae22182214d01f1df164ea3d040c26da5f9a0
Brian Stern <sterno2510@gmail.com>
2024-06-28 13:20:38 -0400

    updated db to hold visit count and display it

--- server/db/db.js
+++ server/db/db.js
@@ -11,6 +11,15 @@ const resumeSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    visitCount: {
+      type: Number,
+      default: 1,
+    },
   },
   {
     timestamps: true,
@@ -19,4 +28,22 @@ const resumeSchema = new mongoose.Schema(
 
 const AiCompanionResume = mongoose.model(process.env.DB_NAME, resumeSchema);
 
-module.exports = AiCompanionResume;
+const saveOrUpdateUser = (username, email) => AiCompanionResume.findOneAndUpdate(
+  { email },
+  {
+    name: username,
+    email,
+    $inc: { visitCount: 1 },
+  },
+  { new: true, upsert: true },
+)
+  .then((updatedUser) => {
+    console.log('User saved or updated:', updatedUser);
+    return updatedUser.visitCount;
+  })
+  .catch((error) => {
+    console.error('Error saving or updating user:', error);
+    throw error;
+  });
+
+module.exports = { saveOrUpdateUser };
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,8 @@
 /* eslint-disable react/function-component-definition */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { useAuth0 } from '@auth0/auth0-react';
+import axios from 'axios';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import Button from '@mui/material/Button';
 import logo from './assets/ailogo.png';
@@ -12,10 +13,24 @@ import {
   SloganStyled,
   LogoutButtonContainerStyled,
   ProfileImageStyled,
+  VisitCounterStyled,
 } from './HeaderStyledComponents';
 
 const Header = () => {
   const { logout, isAuthenticated, user } = useAuth0();
+  const [visitCount, setVisitCount] = useState(null);
+
+  useEffect(() => {
+    if (user) {
+      axios.get(`/update?name=${user.name}&email=${user.email}`)
+        .then((response) => (
+          setVisitCount(response.data)
+        ))
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }, [isAuthenticated]);
 
   const logOut = () => {
     logout({ logoutParams: { returnTo: window.location.origin } });
@@ -28,7 +43,14 @@ const Header = () => {
       {isAuthenticated && (
         <LogoutButtonContainerStyled>
           {user?.picture && (
-            <ProfileImageStyled src={user.picture} alt="Profile" />
+            <>
+              <ProfileImageStyled src={user.picture} alt="Profile" />
+              <VisitCounterStyled>
+                Total Visits:
+                {' '}
+                {visitCount}
+              </VisitCounterStyled>
+            </>
           )}
           <Button onClick={logOut} variant="outlined" href="#outlined-buttons">
             Log Out
