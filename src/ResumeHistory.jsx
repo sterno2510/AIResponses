594a2f86390bb9b72a6835d249a2e8c28099ec97
Brian Stern <sterno2510@gmail.com>
2024-08-14 14:36:13 -0400

    updated resume history ui

--- src/ResumeHistory.jsx
+++ src/ResumeHistory.jsx
@@ -1,33 +1,66 @@
 /* eslint-disable no-underscore-dangle */
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import axios from 'axios';
-import { DangerousHtmlStyled } from './FormStyledComponents';
+import {
+  DangerousHtmlStyled, ButtonStyled, NavButtonContainer, LeftButton, RightButton,
+} from './FormStyledComponents';
 
-const resumeHistory = ({ userObject }) => {
-  console.log('in resume history');
+const ResumeHistory = ({ userObject }) => {
   const [resumeHistories, setResumeHistories] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   const retrieveResumes = () => {
-    // eslint-disable-next-line no-underscore-dangle
     axios.get(`/retrieve-resumes?userId=${userObject._id}`)
       .then((res) => {
         setResumeHistories(res.data.resumes);
-        console.log('res', res);
+        setCurrentIndex(0); // Reset index to 0 when new data is loaded
       })
       .catch((err) => {
         console.log('error retrieving resumes', err);
       });
   };
-  console.log('resume history', resumeHistories);
+
+  const showNextResume = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % resumeHistories.length);
+  };
+
+  const showPreviousResume = () => {
+    setCurrentIndex((prevIndex) => (prevIndex - 1
+       + resumeHistories.length) % resumeHistories.length);
+  };
+
   return (
     <>
-      {resumeHistories.map((resume) => (
-        <DangerousHtmlStyled dangerouslySetInnerHTML={{ __html: resume.newResume }} />
-      ))}
+
       <div>Want to see your history of resumes?</div>
-      <button onClick={retrieveResumes} type="button">Resume History</button>
+      <ButtonStyled onClick={retrieveResumes} type="button">Retrieve Resume History</ButtonStyled>
+      {resumeHistories.length > 0 && (
+        <>
+          <h1>Your Resume History</h1>
+          <NavButtonContainer>
+            {currentIndex > 0 && (
+            <LeftButton onClick={showPreviousResume} type="button">&lt; Previous</LeftButton>
+            )}
+            {currentIndex < resumeHistories.length - 1 && (
+            <RightButton onClick={showNextResume} type="button">Next &gt;</RightButton>
+            )}
+          </NavButtonContainer>
+        </>
+      )}
+      {resumeHistories.length > 0 && (
+        <DangerousHtmlStyled
+          dangerouslySetInnerHTML={{ __html: resumeHistories[currentIndex].newResume }}
+        />
+      )}
     </>
   );
 };
 
-export default resumeHistory;
+ResumeHistory.propTypes = {
+  userObject: PropTypes.shape({
+    _id: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default ResumeHistory;
